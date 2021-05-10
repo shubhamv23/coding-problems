990719841524d2a3d8a6ce17c7a5fd163aea84c9
Shubham Verma <shubhamv59@gmail.com>
2021-05-10 19:05:01 +0530

    add smallest k length subsequence solution

---
+++ src/lexico_smallest_k_length_subsequence/Driver.java
@@ -0,0 +1,47 @@
+package lexico_smallest_k_length_subsequence;
+
+import sun.jvm.hotspot.utilities.Assert;
+
+import java.util.Stack;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class Driver {
+	public static void main(String[] args) {
+		Assert.that(findLexicographicallySmallestString("bbcaab", 3).equals("aab"), "FAILED");
+		Assert.that(findLexicographicallySmallestString("aabdaabc", 3).equals("aaa"), "FAILED");
+		Assert.that(findLexicographicallySmallestString("aabdaabcbbbbbbbbbbbbbbbbbbbbbcccccccccabababababcccccccaaabbbbbbbaaaa", 4).equals("aaaa"), "FAILED");
+	}
+
+	private static String findLexicographicallySmallestString(String input, int requiredLength) {
+		int totalInputLength = input.length();
+		Stack<Character> s = new Stack<>();
+		s.add(input.charAt(0));
+
+		IntStream.range(1, totalInputLength).forEach(currentPos -> {
+			Character currentChar = input.charAt(currentPos);
+			while (!s.isEmpty() &&
+					currentChar < s.peek() &&
+					checkIfEnoughCharRemainingForRequiredLength(s.size(), totalInputLength, requiredLength, currentPos)) {
+				s.pop();
+			}
+			if (s.size() < requiredLength) {
+				s.add(currentChar);
+			}
+		});
+
+		return getStringFromStack(s);
+	}
+
+	private static boolean checkIfEnoughCharRemainingForRequiredLength(int currentIncludedLength, int totalInputLength, int requiredLength, int currentPos) {
+		return currentIncludedLength + (totalInputLength - currentPos) - 1 >= requiredLength;
+	}
+
+	private static String getStringFromStack(Stack<Character> s) {
+		String output = "";
+		return s.stream()
+				.map(c -> output + c)
+				.collect(Collectors.joining());
+	}
+
+}
